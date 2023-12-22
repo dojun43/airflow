fb54c6b20a09c0c265a0920ccc6ae9acb1dee711
dodo <dojun43@naver.com>
2023-12-22 13:17:08 +0900

    dags_python_with_postgres_hook_bulk_load 수정

--- dags/dags_python_with_postgres_hook_bulk_load.py
+++ dags/dags_python_with_postgres_hook_bulk_load.py
@@ -13,13 +13,12 @@ with DAG(
         postgres_hook = PostgresHook(postgres_conn_id)
         postgres_hook.bulk_load(tbl_nm, file_nm)
 
-        insrt_postgres = PythonOperator(
-            task_id='insrt_postgres',
-            python_callable=insrt_postgres,
-            op_kwargs={'postgres_conn_id':'conn-db-postgres-custom',
-                       'tbl_nm':'TbCorona19CountStatus_bulk1',
-                       'file_nm':'/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/TbCorona19CountStatus.csv'
-                       }
-        )
+    insrt_postgres = PythonOperator(
+        task_id='insrt_postgres',
+        python_callable=insrt_postgres,
+        op_kwargs={'postgres_conn_id':'conn-db-postgres-custom',
+                    'tbl_nm':'TbCorona19CountStatus_bulk1',
+                    'file_nm':'/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/TbCorona19CountStatus.csv'
+                    }
+    )
     
-    insrt_postgres
