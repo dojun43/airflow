08756a81ebf1fc1c42218fc97d6b7ed78f84ff9f
dodo <dojun43@naver.com>
2023-12-22 15:59:29 +0900

    dags_python_with_custom_hook_bulk_load 수정

--- dags/dags_python_with_custom_hook_bulk_load.py
+++ dags/dags_python_with_custom_hook_bulk_load.py
@@ -18,6 +18,6 @@ with DAG(
         python_callable=insrt_postgres,
         op_kwargs={'postgres_conn_id': 'conn-db-postgres-custom',
                 'tbl_nm': 'TbCorona19CountStatus_bulk2',
-                'file_nm': '/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/odash}}/TbCorona19CountSt.csv'
+                'file_nm':'/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/TbCorona19CountStatus.csv'
                 }
     )
