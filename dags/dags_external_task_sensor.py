bf2816502217fa7efdf68ea63e9eeae680e14c9a
dodo <dojun43@naver.com>
2023-12-23 18:42:29 +0900

    dags_external_task_sensor 수정

--- dags/dags_external_task_sensor.py
+++ dags/dags_external_task_sensor.py
@@ -32,7 +32,7 @@ with DAG(
         task_id='external_task_sensor_c',
         external_dags_id='dags_branch_python_operator',
         external_task_id='task_c',
-        allowed_states=[State.SKIPPED],
+        allowed_states=[State.SUCCESS],
         execution_delta=timedelta(hours=6),
         poke_interval=10
     )
