e383c74a183eac91a7ea82019339a9e3886f7edb
dodo <dojun43@naver.com>
2023-12-27 17:14:48 +0900

    upbit_api 업로드

---
+++ dags/dags_upbit_api_candles.py
@@ -0,0 +1,13 @@
+from operators.upbit_api_to_csv_operator import UpbitApiToCsvOperator
+from airflow import DAG
+import pendulum
+
+with DAG(
+    dag_id='dags_seoul_api_corona',
+    schedule='0 7 * * *',
+    start_date=pendulum.datetime(2023,12,1, tz='Asia/Seoul'),
+    catchup=False
+) as dag:
+        t1 = UpbitApiToCsvOperator(
+        task_id='t1'
+    )
---
+++ plugins/operators/upbit_api_to_csv_operator.py
@@ -0,0 +1,24 @@
+from airflow.models.baseoperator import BaseOperator
+from airflow.hooks.base import BaseHook
+import pandas as pd 
+
+class SeoulApiToCsvOperator(BaseOperator):
+    template_fields = ('endpoint', 'path','file_name','base_dt')
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    
+    def execute(self, context):
+        import requests
+        import json 
+
+        request_url = "https://api.upbit.com/v1/candles/minutes/1?market=KRW-BTC&count=5"
+
+        headers = {"accept": "application/json"}
+
+        response = requests.get(request_url, headers)
+        contents = json.loads(response.text)
+
+        row_df = pd.DataFrame(contents)
+
+        self.log.info(row_df)
