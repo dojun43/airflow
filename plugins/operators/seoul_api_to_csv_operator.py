1515ce572b8ab0e2fc449e013f2dd9967a4bb400
dodo <dojun43@naver.com>
2023-12-27 13:44:32 +0900

    seoul_api_to_csv_operator 수정

--- plugins/operators/seoul_api_to_csv_operator.py
+++ plugins/operators/seoul_api_to_csv_operator.py
@@ -28,6 +28,7 @@ class SeoulApiToCsvOperator(BaseOperator):
             self.log.info(f'끝:{end_row}')
             row_df = self._call_api(self.base_url, start_row, end_row)
             total_row_df = pd.concat([total_row_df, row_df])
+            self.log.info(f'len:{len(row_df)}')
             if len(row_df) < 1000:
                 break
             else:
