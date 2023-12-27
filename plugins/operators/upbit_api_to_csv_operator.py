cf4ccdf38b282d68b060f4aa5be2f649903ddb1e
dodo <dojun43@naver.com>
2023-12-27 17:50:54 +0900

    upbit_api 업로드

--- plugins/operators/upbit_api_to_csv_operator.py
+++ plugins/operators/upbit_api_to_csv_operator.py
@@ -10,15 +10,24 @@ class UpbitApiToCsvOperator(BaseOperator):
     
     def execute(self, context):
         import requests
+        from urllib.request import urlopen
         import json 
 
         request_url = "https://api.upbit.com/v1/candles/minutes/1?market=KRW-BTC&count=5"
 
-        headers = {"accept": "application/json"}
+        try:
+            headers = {"accept": "application/json"}
 
-        response = requests.get(request_url, headers)
-        contents = json.loads(response.text)
+            response = requests.get(request_url, headers)
+            contents = json.loads(response.text)
 
-        row_df = pd.DataFrame(contents)
+            row_df = pd.DataFrame(contents)
+
+        except:
+            page = urlopen(request_url)
+
+            doc = page.read().decode('utf-8')
+            dic = json.loads(doc)
+            row_df = pd.DataFrame(dic)
 
         self.log.info(row_df)
